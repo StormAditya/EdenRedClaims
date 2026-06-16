e0414d914670e28fce58cacc1fd1961e1dafa33e
StormAditya <desaiaditya509@gmail.com>
2026-06-16 18:33:13 +0530

    Fix import statement for removeUser in admin router

--- server/routes/router-admin.js
+++ server/routes/router-admin.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const router = express.Router()
-<<<<<<< HEAD
-const { getUser, updateUser } = require('../controllers/controller-admin')
-=======
-const {getUser, removeUser} = require('../controllers/controller-admin')
->>>>>>> 7a548f902dafd1cdf58adfec2faf7326b4148592
+const { getUser, updateUser, removeUser } = require('../controllers/controller-admin')
 
 router.get('/users', getUser)
 router.patch('/users',updateUser)
-
 router.delete('/users/:id', removeUser)
 
 module.exports = router
