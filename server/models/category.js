46a214493b7c4c972d1bf1bfbdab2ca0faa602e5
StormAditya <desaiaditya509@gmail.com>
2026-06-16 11:37:52 +0530

    category model

---
+++ server/models/category.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+
+  category_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  category_name: {
+    type: DataTypes.STRING(60),
+    allowNull: false,
+    unique: true,
+    validate: {
+        notEmpty:{
+            msg:'Category Name is required'
+        },
+        notNull: {
+            msg: 'Name is reqd'
+        }
+    }
+  }
+}, {
+  tableName: 'Category'
+});
+
+module.exports = User;
