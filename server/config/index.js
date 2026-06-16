44bde44ffc760dc84a3c487d210228753dcfb1ca
N0xx77 <abodhdpanchal@gmail.com>
2026-06-16 12:58:40 +0530

    Add foreign key relationship between Claims and Category models

--- server/config/index.js
+++ server/config/index.js
@@ -10,12 +10,16 @@ const app = express()
 
 User.hasMany(Claims, { foreignKey: 'user_id', onDelete: 'CASCADE' });
 Claims.belongsTo(User, { foreignKey: 'user_id' });
-
 Status.hasMany(Claims, {foreignKey: 'status_id', onDelete: 'CASCADE' });
 Claims.belongsTo(Status, {foreignKey: 'status_id'});
+Category.hasMany(Claims, {foreignKey: 'category_id', onDelete: 'CASCADE'})
+Claims.belongsTo(Category, {foreignKey: 'category_id'})
 
 Receipt.hasOne(Claims, {foreignKey: 'claim_id', onDelete: 'CASCADE'})
 Claims.belongsTo(Receipt, {foreignKey: 'claim_id'})
+
+//ADD a foregin key in ITEMS:
+//REFERENCING: RECEIPT_ID
 
 sequelize.sync().then(() => {
     console.log('Database connected and synchronised...')
