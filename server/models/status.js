46677f23d470f649b450430610cd8b432e1918bc
StormAditya <desaiaditya509@gmail.com>
2026-06-16 11:46:51 +0530

    Status Model

---
+++ server/models/status.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Status = sequelize.define('Status', {
+
+  status_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  status_name: {
+    type: DataTypes.STRING(60),
+    allowNull: false,
+    unique: true,
+    validate: {
+        notEmpty:{
+            msg:'Status Name is required'
+        },
+        notNull: {
+            msg: 'Status Name is reqd'
+        }
+    }
+  }
+}, {
+  tableName: 'Status'
+});
+
+module.exports = Status;
