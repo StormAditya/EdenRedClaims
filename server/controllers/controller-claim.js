321dfa409503ec29e5fe347599c40eb494798915
N0xx77 <abodhdpanchal@gmail.com>
2026-06-17 14:56:43 +0530

    feat: Enhance claims management with updated claim handling and error responses

--- server/controllers/controller-claim.js
+++ server/controllers/controller-claim.js
@@ -1,14 +1,13 @@
 const claims = require('../models/claims')
 const createClaim = async (req, res) => {
     try{
-        const { user_id, status_id, category_id, description, claim_amount ,submission_date} = req.body
+        const { user_id, status_id, category_id, description, claim_amount} = req.body
         const newClaim = await claims.create({
             user_id: user_id,
             status_id: status_id,
             category_id: category_id,
             description: description,
             claim_amount: claim_amount,
-            submission_date: submission_date
         })
         res.status(200).json({success:true, data: newClaim})
     }
@@ -29,8 +28,83 @@ const getClaims = async (req,res) => {
     
 }
 
+const updateClaimEmployee = async (req, res) => {
+    try{
+        const { claim_id, category_id, description, claim_amount } = req.body
+        
+        const [updatedRows] = await claims.update(
+            {
+                category_id: category_id,
+                description: description,
+                claim_amount: claim_amount,
+            },
+            {
+                where: {
+                    id: claim_id
+                }
+            }
+        )
+
+        if( updatedRows === 0) return res.status(404).json({success: false, msg: 'Claim_ID not found'})
+        
+        return res.status(200).json({success: true, data: updatedRows})
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).json({success: false, msg: "Error updating claims"})
+    }
+}
+
+const updateClaimAdmin = async (req, res) => {
+    try{
+        const { claim_id, status_id } = req.body
+
+        const [updatedRows] = await claims.update(
+            {
+                status_id: status_id
+            },
+            {
+                where: {
+                    id: claim_id
+                }
+            }
+        )
+
+        if(updatedRows === 0){
+            return res.status(404).json({success: false, msg: 'Claim_ID not found'})
+        }
+
+        return res.status(200).json({success: true, data: updatedRows})
+    }
+    catch(err) {
+        console.error(err)
+        return res.status(500).json({success: false, msg: 'Cannot Update Claim'})
+    }
+    
+}
+
+const removeClaim = async (req, res) => {
+    const { claim_id } = req.params
+    try{
+        const removedClaim = claims.destroy({
+            where: {
+                id: claim_id
+            }
+        })
+
+        res.status(200).json({success: true, msg: 'Claim deleted'})
+    }
+    catch(err) {
+        console.error(err)
+        res.status(500).json({success: false, msg: 'Claim not found'})
+    }
+}
+
 
 module.exports = {
     createClaim,
-    getClaims
+    getClaims,
+    updateClaimAdmin,
+    updateClaimEmployee,
+    removeClaim
 }
