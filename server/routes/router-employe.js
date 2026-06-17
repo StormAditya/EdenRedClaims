27752c3be159962f4a9db05a7006d04acafa20db
StormAditya <desaiaditya509@gmail.com>
2026-06-17 14:56:12 +0530

    feat: Add receipt management functions for finding, adding, and deleting receipts

--- server/routes/router-employe.js
+++ server/routes/router-employe.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { createClaim } = require('../controllers/controller-claim')
-
+const { findReceiptInfo, addReceiptInfo, deleteReceiptInfo} = require('../controllers/controller-receipt')
 router.post('/claims', createClaim)
+router.post('/receipts/find',findReceiptInfo)
+router.post('/receipts/add',addReceiptInfo)
+router.delete('/receipts/:id', deleteReceiptInfo)
 module.exports = router
