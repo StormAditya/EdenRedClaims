aca214b97a29bf89795cbb6b0146ed1c7c06bb9f
N0xx77 <abodhdpanchal@gmail.com>
2026-06-18 13:39:03 +0530

    feat: Add authentication middleware for user and admin validation

---
+++ server/utils/authentication.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+const isAuth = (req, res, next) => {  
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'somethingsecret',
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: 'Invalid Token' });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: 'No Token' });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: 'Invalid Admin Token' });
+  }
+};
+
+module.exports = {isAdmin, isAuth};
