a58c3e97eb71070c6a928b064ebd63aa0f47336b
StormAditya <desaiaditya509@gmail.com>
2026-06-19 12:42:10 +0530

    refactor: remove unused logout function from auth.js

--- client/auth.js
+++ client/auth.js
@@ -2,8 +2,3 @@ export const getAuthHeader = () => {
   const token = localStorage.getItem('token');
   return token ? { Authorization: `Bearer ${token}` } : {};
 };
-
-export const logout = () => {
-  localStorage.removeItem('token');
-  window.location.href = '/login';
-};
